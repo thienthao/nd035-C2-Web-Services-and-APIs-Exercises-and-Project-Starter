5c3ca015f75e9a1ebe66de1de14edf183be92c46
thienthao <tranthienthao2710@gmail.com>
2022-09-06 20:58:42 +0700

    add unit test for pricing-service

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,52 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.service.PricingService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.mockStatic;
 
 @ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
 @SpringBootTest
 public class PricingServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private static MockMvc mockMvc;
 
+    @BeforeAll
+    static void init() {
+        try (MockedStatic<PricingService> mocked = mockStatic(PricingService.class)) {
+            mocked.when(() -> PricingService.getPrice(1L))
+                    .thenReturn(new Price("USD", new BigDecimal("13779.96"), 1L));
+        }
+    }
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void test_get_vehicle_price() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/services/price?vehicleId=1"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("vehicleId").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("price").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("currency").value("USD"))
+                .andReturn();
+    }
 }
